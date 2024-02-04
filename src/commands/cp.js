d4aa7cdcf6bc3abce0d6e66f26b06d01f44c3cee
Dmitry Khomichenko <dmitrii.khomichenko@pobeda.aero>
2024-02-04 20:08:10 +0300

    refactor: copy

--- src/commands/cp.js
+++ src/commands/cp.js
@@ -1,11 +1,10 @@
 import { createReadStream, createWriteStream } from "fs"
-import { resolve, basename, dirname } from "path"
+import { resolve, basename } from "path"
 import { pipeline } from "stream/promises"
 
 export const cp = async (currentPath, pathToFile, pathToNewDirectory) => {
   const resolvedOldPathToFile = resolve(currentPath, pathToFile)
   const fileName = basename(resolvedOldPathToFile)
-  // const dirPath = dirname(resolvedOldPathToFile)
 
   const resolvedNewPath = resolve(currentPath, pathToNewDirectory, fileName)
 
